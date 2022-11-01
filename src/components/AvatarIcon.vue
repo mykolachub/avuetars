369bb0c78860f1a498a94234352c7d96d04ea601
Nikolai <nikolaichub.me@gmail.com>
2022-11-01 18:57:00 +0200

    feat: svg template and generating avatar

--- src/components/AvatarIcon.vue
+++ src/components/AvatarIcon.vue
@@ -1,7 +1,69 @@
-<template><p>Sussybaka</p></template>
+<template>
+  <svg
+    :viewBox="`0 0 ${$options.SIZE} ${$options.SIZE}`"
+    fill="none"
+    role="img"
+    :width="width"
+    :height="height"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <mask
+      id="mask"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      :width="$options.SIZE"
+      :height="$options.SIZE"
+    >
+      <rect
+        :width="$options.SIZE"
+        :height="$options.SIZE"
+        fill="#FFFFFF"
+        :rx="roundness ? 160 : 0"
+      />
+    </mask>
+    <g mask="url(#mask)" v-html="content"></g>
+  </svg>
+</template>
 
 <script>
-export default {};
+import Avatar from '../lib/avatar.js';
+export default {
+  props: {
+    width: {
+      type: Number, // 48px
+    },
+    height: {
+      type: Number,
+    },
+    density: {
+      type: Number,
+    },
+    type: {
+      type: String, // grid
+    },
+    colors: {
+      type: Array,
+    },
+    roundness: {
+      type: Boolean, // true
+    },
+  },
+
+  SIZE: 80,
+
+  data() {
+    return {
+      content: '',
+    };
+  },
+
+  created() {
+    const { width, height, type, colors, roundness, density } = this;
+    const avatar = new Avatar(width, height, type, colors, roundness, density);
+    this.content = avatar.generate();
+  },
+};
 </script>
 
 <style></style>
